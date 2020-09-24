0dd643f8c25da67d9d88324f04618a75ce3e0335
Julia-IO <tobetranslated01@gmail.com>
2020-09-24 16:50:05 +0000

    Legal files styly added

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 51.896893, lng: -8.486316 },
+        { lat: 41.387920, lng: 2.169920 },
+        { lat: 40.416775, lng: -3.703790 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
